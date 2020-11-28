69e773c98869fb722f6e03c486f0a9633b8fcf56
Kelvin Lopes <kelvin7android@gmail.com>
2020-11-28 18:37:29 -0300

    Updated SignIn, Add Fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SingIn: React.FC = () => {
   return (
-    <Container />
+    <Container>
+      <Image source={logoImg} />
+      <Title> Faça seu logon </Title>
+    </Container>
   );
 }
 
