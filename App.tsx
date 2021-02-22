96dcc3648a85f76ebea147e02374466b2b2d3b23
KelvinLopes <kelvin7android@gmail.com>
2021-02-22 00:08:09 -0300

    Updates

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+   }, []);
+
+  return <View />
+}
+
+export default App;
